18ed9ede14a5f3b967b201424e28ffc4315df60e
Amy Bridges <109692527+abridgesmm@users.noreply.github.com>
2023-01-31 18:30:36 -0500

    Create helpers.php

---
+++ lib/helpers.php
@@ -0,0 +1,92 @@
+<?php
+
+function delete_wp_media($id) {
+
+    return wp_delete_attachment($id, true);
+
+}
+
+function endsWithNumber($string){
+
+    $len = strlen($string);
+
+    if($len === 0){
+        return false;
+    }
+
+    return is_numeric($string[$len-1]);
+}
+
+function formatBytes($size, $precision = 2) {
+
+    $base = log($size, 1024);
+    $suffixes = array('', 'K', 'M', 'G', 'T');   
+
+    return round(pow(1024, $base - floor($base)), $precision) .' '. $suffixes[floor($base)];
+
+}
+
+function runMediaReplace($duplicated_file, $orginal_file, $content, $type) {
+
+    if(str_contains($type, "image/")) {
+
+      $content =  preg_replace("/<!-- wp:image {\"id\":".$duplicated_file['ID']."/", "<!-- wp:image {\"id\":".$orginal_file['ID'], $content);
+      $content =  preg_replace("/class=\"wp-image-".$duplicated_file['ID']."/","/class=\"wp-image-".$orginal_file['ID']."/", $content);
+      return str_replace($duplicated_file['guid'], $orginal_file['guid'], $content);
+
+    }
+
+}
+
+function get_posts_by_attachment_id( $attachment_id ) {
+    
+    $used_as_thumbnail = array();
+  
+    if ( wp_attachment_is_image( $attachment_id ) ) {
+      $thumbnail_query = new WP_Query( array(
+        'meta_key'       => '_thumbnail_id',
+        'meta_value'     => $attachment_id,
+        'post_type'      => 'any',	
+        'fields'         => 'ids',
+        'no_found_rows'  => true,
+        'posts_per_page' => -1,
+      ) );
+  
+      $used_as_thumbnail = $thumbnail_query->posts;
+    }
+  
+    $attachment_urls = array( wp_get_attachment_url( $attachment_id ) );
+  
+    if ( wp_attachment_is_image( $attachment_id ) ) {
+      foreach ( get_intermediate_image_sizes() as $size ) {
+        $intermediate = image_get_intermediate_size( $attachment_id, $size );
+        if ( $intermediate ) {
+          $attachment_urls[] = $intermediate['url'];
+        }
+      }
+    }
+  
+    $used_in_content = array();
+  
+    foreach ( $attachment_urls as $attachment_url ) {
+      $content_query = new WP_Query( array(
+        's'              => $attachment_url,
+        'post_type'      => 'any',	
+        'fields'         => 'ids',
+        'no_found_rows'  => true,
+        'posts_per_page' => -1,
+      ) );
+  
+      $used_in_content = array_merge( $used_in_content, $content_query->posts );
+    }
+  
+    $used_in_content = array_unique( $used_in_content );
+  
+    $posts = array(
+      'thumbnail' => $used_as_thumbnail,
+      'content'   => $used_in_content,
+    );
+  
+    return $posts;
+
+  }
